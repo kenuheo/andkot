8a4295eb9c2d89e0e81a8b6adadbb8870eaab3ba
kenuheo <kenu@okky.kr>
2023-06-29 14:50:52 +0900

    Refresh Button

--- myword/src/main/java/com/example/myword/MainActivity.kt
+++ myword/src/main/java/com/example/myword/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.myword
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val textView = findViewById<TextView>(R.id.textView)
         textView.text = getWord()
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            textView.text = getWord()
+        }
     }
 
     private fun getWord(): String {
