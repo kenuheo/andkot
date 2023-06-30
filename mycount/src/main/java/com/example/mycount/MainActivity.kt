f2deb1ea010ecb9fe8e057bfae812be1eaad16b4
kenuheo <kenu@okky.kr>
2023-06-30 15:53:00 +0900

    5cho

--- mycount/src/main/java/com/example/mycount/MainActivity.kt
+++ mycount/src/main/java/com/example/mycount/MainActivity.kt
@@ -2,31 +2,35 @@ package com.example.mycount
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.mycount.databinding.ActivityMainBinding
-import com.example.mycount.ui.theme.AndKotTheme
 
 class MainActivity : ComponentActivity() {
     var count = 0
+    var initTime = 0L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.countButton.setOnClickListener {
-            count++
-            binding.countText.text = "$count"
+            if (checkTime()) {
+                count++
+                binding.countText.text = "$count"
+                binding.textView.text = "${System.currentTimeMillis() - initTime}"
+            }
         }
         binding.resetButton.setOnClickListener {
             count = 0
             binding.countText.text = "$count"
         }
+    }
+
+    private fun checkTime(): Boolean {
+        // 30초 이내인가
+        if (count == 0) {
+            initTime = System.currentTimeMillis()
+        }
+        var timeDiff = System.currentTimeMillis() - initTime
+        return timeDiff < 5 * 1000
     }
 }
